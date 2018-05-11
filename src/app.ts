c997c648d2f6a81abde238b6726f5bf09472d721
Nathan Lafferty <lcgamer333@gmail.com>
2018-05-11 12:59:08 -0500

    added test for arbitrary multiple canvases

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,23 @@ class App {
 	private ClipboardManager: ClipboardManager = new ClipboardManager();
 
 	Run(): void {
-		let canvasListener = new CanvasContext('prototypeCanvas');
-		this.ClipboardManager.SubscribeContext(canvasListener);
-		let canvasElement: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('prototypeCanvas');
+		let canvasListener1 = new CanvasContext('prototypeCanvas1');
+		let canvasListener2 = new CanvasContext('prototypeCanvas2');
+		this.ClipboardManager.SubscribeContext(canvasListener1);
+		this.ClipboardManager.SubscribeContext(canvasListener2);
+		let canvasElement: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('prototypeCanvas1');
+		canvasElement.onmousedown = (ev: MouseEvent) => {
+			if(ev.shiftKey) {
+				this.ClipboardManager.OnInternalCopy();
+			}
+			if(ev.ctrlKey) {
+				this.ClipboardManager.OnInternalCut();
+			}
+			if(ev.altKey) {
+				this.ClipboardManager.OnInternalPaste();
+			}
+		};
+		canvasElement = <HTMLCanvasElement>document.getElementById('prototypeCanvas2');
 		canvasElement.onmousedown = (ev: MouseEvent) => {
 			if(ev.shiftKey) {
 				this.ClipboardManager.OnInternalCopy();
