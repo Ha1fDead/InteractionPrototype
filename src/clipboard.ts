05ed94b0dae60165fdc62457f074f389c132c9d3
Nathan Lafferty <lcgamer333@gmail.com>
2018-05-11 16:14:20 -0500

    removed redundant code

--- src/clipboard.ts
+++ src/clipboard.ts
@@ -17,57 +17,6 @@ export enum DataTransferTypes {
 	Text = 'text/plain'
 }
 
-export class CanvasContext implements IInterfaceContext {
-	private pasteHistory: string[] = [];
-	constructor(public Id: string) {
-
-	}
-
-    /**
-     * The element's custom handled 'Cut' operation
-     *
-     * This method should REMOVE the element via a command (so the user can undo it) and return a DataTransfer object representing the data entirely so it can be replicated
-     */
-	HandleCut(): DataTransfer {
-		let data = new DataTransfer();
-		data.setData(DataTransferTypes.Text, 'WOH SOME TEXT HERE');
-		return data;
-	}
-    /**
-     * The element's custom handled 'Copy' operation
-     *
-     * This method should make a copy of the data in a DataTransfer object so it can be replicated at-will from the user
-     */
-	HandleCopy(): DataTransfer {
-		let data = new DataTransfer();
-		data.setData(DataTransferTypes.Text, 'WOH SOME TEXT HERE');
-		return data;
-	}
-    /**
-     * The element's custom handled 'Paste' operation
-     *
-     * This method should build a "Paste Event" into the command stack so the user can undo this.
-     * @param data to be pasted
-     */
-	HandlePaste(data: DataTransfer): void {
-		this.pasteHistory.push(data.getData(DataTransferTypes.Text));
-		// quick and dirty paste demonstration
-		let canvas = <HTMLCanvasElement>document.getElementById(this.Id);
-		let canvasCtx = <CanvasRenderingContext2D>canvas.getContext("2d");
-		canvasCtx.clearRect(0, 0, 400, 600);
-		let idx = 0;
-		for (let paste of this.pasteHistory) {
-			canvasCtx.fillText(paste, 50, idx * 50, 40);
-			idx++;
-		}
-	}
-}
-
-// Do I need to have an internal copy if I can trigger document.exec(copy) ??
-	// are there permissions associated with it?
-	// otherwise internal copy can just be document.exec(copy) and then I get an external copy event
-	// for paste, maybe the same thing
-
 /**
  * There should only ever be a single 'Clipboard' object in the entire application. Unlike undo/redo, the clipboard should always reference the same data.
  * 
